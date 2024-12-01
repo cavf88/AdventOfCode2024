a55cf48971c60086c111d761fd1307ee6a1eb181
unknown <cavf88@hotmail.com>
2024-12-01 13:45:57 -0500

    Solution for day1 completed

--- Advent_of_code_2024/Day1/Puzzle1/main.cpp
+++ Advent_of_code_2024/Day1/Puzzle1/main.cpp
@@ -1,27 +1,41 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 using namespace std;
+
+const static int NUM_SIZE = 5;
 
 int main()
 {
     string inputLine;
     fstream inputFile("input.txt");
+    int sum = 0;
+    vector<int> n1, n2;
     while(getline(inputFile, inputLine))
     {
         // Format of line is a string of numbers followed by 3 spaces and then the second number
-        size_t pos = inputLine.find("   ");
+        size_t pos = inputLine.find(" ");
         const string firstNumberStr = inputLine.substr(0, pos);
         // assume there is always 3 spaces between the string of numbers to be able to get the second number
         const string secondNumberStr = inputLine.substr(pos + 3, inputLine.length());
 
-        // convert string of numebers to int
-        const int n1 = atoi(firstNumberStr.c_str());
-        const int n2 = atoi(secondNumberStr.c_str());
-        cout << firstNumberStr << "+" << secondNumberStr << endl;
+        n1.push_back(atoi(firstNumberStr.c_str()));
+        n2.push_back(atoi(secondNumberStr.c_str()));
     }
+
+        sort(n1.begin(), n1.end());
+        sort(n2.begin(), n2.end());
+
+        for(int i = 0; i < n1.size(); i++)
+        {
+            sum += abs(n1[i] - n2[i]);
+        }
+
+    cout << sum << endl;
     inputFile.close();
-    
+
     return 0;
 }
