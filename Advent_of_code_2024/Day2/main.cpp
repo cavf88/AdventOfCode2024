dffadff7ad3d2f01f6e21efc15e08940513e347e
unknown <cavf88@hotmail.com>
2024-12-02 10:16:26 -0500

    Day2: adds parser for lines into vector

--- Advent_of_code_2024/Day2/main.cpp
+++ Advent_of_code_2024/Day2/main.cpp
@@ -7,7 +7,39 @@
 
 using namespace std;
 
+vector<int> parseLine(const string& line, const char* delimiter = " ")
+{
+    vector<int> parsedLine;
+
+    size_t pos = 0;
+    while(1)
+    {
+        auto newPos = line.find(delimiter, pos);
+        const string tempStr = line.substr(pos, 2);
+        parsedLine.push_back(atoi(tempStr.c_str()));
+        pos = newPos+1;
+
+        if(newPos == string::npos)
+        {
+            break;
+        }
+    }
+
+    return parsedLine;
+}
+
 int main()
 {
+
+    string inputLine;
+    fstream inputFile("input.txt");
+
+    while(getline(inputFile, inputLine))
+    {
+        vector<int> values = parseLine(inputLine);
+    }
+
+    inputFile.close();
+
     return 0;
 }
