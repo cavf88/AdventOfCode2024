93d84711548dbd352a379939236952b5d475bc15
unknown <cavf88@hotmail.com>
2024-12-02 08:25:34 -0500

    Day1: cleanup

--- Advent_of_code_2024/Day1/Puzzle1/main.cpp
+++ Advent_of_code_2024/Day1/Puzzle1/main.cpp
@@ -7,39 +7,19 @@
 
 using namespace std;
 
-const static int NUM_SIZE = 5;
-
-int main()
+void part1(const vector<int>& n1, const vector<int> n2)
 {
-    string inputLine;
-    fstream inputFile("input.txt");
     int sum = 0;
-    vector<int> n1, n2;
-    while(getline(inputFile, inputLine))
-    {
-        // Format of line is a string of numbers followed by 3 spaces and then the second number
-        size_t pos = inputLine.find(" ");
-        const string firstNumberStr = inputLine.substr(0, pos);
-        // assume there is always 3 spaces between the string of numbers to be able to get the second number
-        const string secondNumberStr = inputLine.substr(pos + 3, inputLine.length());
-
-        n1.push_back(atoi(firstNumberStr.c_str()));
-        n2.push_back(atoi(secondNumberStr.c_str()));
-    }
-
-    sort(n1.begin(), n1.end());
-    sort(n2.begin(), n2.end());
-
-    // Part 1
     for(int i = 0; i < n1.size(); i++)
     {
         sum += abs(n1[i] - n2[i]);
     }
 
     cout << sum << endl;
+}
 
-    // Part 2
-
+void part2(const vector<int>& n1, const vector<int> n2)
+{
     map<int, int> resultMap;
     for(int i = 0; i < n1.size(); i++)
     {
@@ -54,15 +34,41 @@ int main()
         }
     }
 
-    int sum2 = 0;
+    int sum = 0;
     for(int i = 0; i < n1.size(); i++)
     { 
-        sum2 += resultMap[n1[i]] * n1[i];
+        sum += resultMap[n1[i]] * n1[i];
     }
 
-    cout << sum2 << endl;
+    cout << sum << endl;
+}
+
+int main()
+{
+    string inputLine;
+    fstream inputFile("input.txt");
+
+    vector<int> n1, n2;
+
+    while(getline(inputFile, inputLine))
+    {
+        // Format of line is a string of numbers followed by 3 spaces and then the second number
+        size_t pos = inputLine.find(" ");
+        const string firstNumberStr = inputLine.substr(0, pos);
+        // assume there is always 3 spaces between the string of numbers to be able to get the second number
+        const string secondNumberStr = inputLine.substr(pos + 3, inputLine.length());
+
+        n1.push_back(atoi(firstNumberStr.c_str()));
+        n2.push_back(atoi(secondNumberStr.c_str()));
+    }
 
     inputFile.close();
+
+    sort(n1.begin(), n1.end());
+    sort(n2.begin(), n2.end());
+
+    part1(n1, n2);
+    part2(n1, n2);
 
     return 0;
 }
