32ab6a80bf69c67c73970419f991301911a5335e
unknown <cavf88@hotmail.com>
2024-12-02 10:47:13 -0500

    Day2: solution

--- Advent_of_code_2024/Day2/main.cpp
+++ Advent_of_code_2024/Day2/main.cpp
@@ -7,6 +7,8 @@
 
 using namespace std;
 
+const static int MAX_DIFF = 3;
+
 vector<int> parseLine(const string& line, const char* delimiter = " ")
 {
     vector<int> parsedLine;
@@ -15,7 +17,7 @@ vector<int> parseLine(const string& line, const char* delimiter = " ")
     while(1)
     {
         auto newPos = line.find(delimiter, pos);
-        const string tempStr = line.substr(pos, 2);
+        const string tempStr = line.substr(pos, newPos-pos);
         parsedLine.push_back(atoi(tempStr.c_str()));
         pos = newPos+1;
 
@@ -28,17 +30,48 @@ vector<int> parseLine(const string& line, const char* delimiter = " ")
     return parsedLine;
 }
 
+bool isVectorValidPart1(const vector<int>& values)
+{
+    int n1 = values[0];
+    int n2 = values[1];
+    int diff = n1 - n2;
+    if(abs(diff)> MAX_DIFF)
+        return false;
+
+    if(diff == 0)
+        return false;
+
+    for(int i = 1; i < values.size()-1; i++)
+    {
+        n1 = values[i];
+        n2 = values[i+1];
+        int newDiff = n1-n2;
+        if(diff > 0 && newDiff < 0 || diff < 0 && newDiff > 0 || newDiff == 0 || abs(newDiff)> MAX_DIFF)
+            return false;
+
+        diff = newDiff;
+    }
+
+    return true;
+}
+
 int main()
 {
 
     string inputLine;
     fstream inputFile("input.txt");
+    int part1Sum = 0;
 
     while(getline(inputFile, inputLine))
     {
         vector<int> values = parseLine(inputLine);
+        if(isVectorValidPart1(values))
+        {
+            part1Sum++;
+        }
     }
 
+    cout << "Part1: " << part1Sum << endl;
     inputFile.close();
 
     return 0;
