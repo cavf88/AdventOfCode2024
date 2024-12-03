e6a291db013034bba264c91ea3450884cab92cc8
unknown <cavf88@hotmail.com>
2024-12-02 23:08:38 -0500

    Part2 Incomplete

--- Advent_of_code_2024/Day2/main.cpp
+++ Advent_of_code_2024/Day2/main.cpp
@@ -30,27 +30,42 @@ vector<int> parseLine(const string& line, const char* delimiter = " ")
     return parsedLine;
 }
 
-bool isVectorValidPart1(const vector<int>& values)
+bool isVectorValid(vector<int>& values, const int levelOfTolerance = 0)
 {
-    int n1 = values[0];
-    int n2 = values[1];
-    int diff = n1 - n2;
-    if(abs(diff)> MAX_DIFF)
-        return false;
 
-    if(diff == 0)
-        return false;
-
-    for(int i = 1; i < values.size()-1; i++)
+    int localLevelOfTolerance = 0;
+    int pos1 = 0;
+    int pos2 = 1;
+    do
     {
-        n1 = values[i];
-        n2 = values[i+1];
+        int n1 = values[pos1];
+        int n2 = values[pos2];
+        int diff = n1 - n2;
+        n1 = values[pos1+1], n2 = values[pos2+1];
         int newDiff = n1-n2;
-        if(diff > 0 && newDiff < 0 || diff < 0 && newDiff > 0 || newDiff == 0 || abs(newDiff)> MAX_DIFF)
-            return false;
 
+        if(diff > 0 && newDiff < 0 || diff < 0 && newDiff > 0 || newDiff == 0 || abs(newDiff)> MAX_DIFF || abs(diff) > MAX_DIFF || diff == 0)
+        {
+            if(localLevelOfTolerance == levelOfTolerance)
+            {
+                return false;
+            }
+            else
+            {
+                // incomplete for part 2... missing values. The answwer is 528
+                values.erase(values.begin() + pos2 + 1);
+                pos1 = 0;
+                pos2 = 1;
+                localLevelOfTolerance++;
+                continue;
+            }
+        }
+        
         diff = newDiff;
-    }
+        pos1++;
+        pos2++;
+        
+    } while (pos1 < values.size()-2);
 
     return true;
 }
@@ -61,17 +76,31 @@ int main()
     string inputLine;
     fstream inputFile("input.txt");
     int part1Sum = 0;
+    int part2Sum = 0;
 
     while(getline(inputFile, inputLine))
     {
         vector<int> values = parseLine(inputLine);
-        if(isVectorValidPart1(values))
+        // part1
+        if(isVectorValid(values))
         {
             part1Sum++;
         }
+        
+        //par2
+        if(isVectorValid(values, 1))
+        {
+            part2Sum++;
+        }
+        else
+        {
+            cout << inputLine << endl;
+        }
+        
     }
 
     cout << "Part1: " << part1Sum << endl;
+    cout << "Part2: " << part2Sum << endl;
     inputFile.close();
 
     return 0;
