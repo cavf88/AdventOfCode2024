8cfbcbf0f15da0e0d29d9b3ea5fd087b5a4eb512
unknown <cavf88@hotmail.com>
2024-12-05 09:51:15 -0500

    input parser for part 1 finished

--- Advent_of_code_2024/Day5/main.cpp
+++ Advent_of_code_2024/Day5/main.cpp
@@ -2,10 +2,61 @@
 #include <string>
 #include <map>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
+typedef map<int, vector<int>> Rules;
+typedef vector<vector<int>> Pages;
+
+void parseLineInRules(const string& line, Rules& rules)
+{
+    int n1 = 0;
+    int n2 = 0;
+    sscanf(line.c_str() ,"%d|%d", &n1, &n2);
+    rules[n1].push_back(n2);
+}
+
+void parseLine(const string& line, Pages& pages)
+{
+    int pos = 0;
+    vector<int> pageNums;
+    do
+    {
+        int foundIndex = line.find(',', pos);
+        const int pageNum = atoi(line.substr(pos, foundIndex).c_str());
+        pageNums.push_back(pageNum);
+        pos = foundIndex + 1;
+    }while(pos != 0);
+    pages.push_back(pageNums);
+}
+
 int main()
 {
+
+    string inputLine;
+    fstream inputFile("input_test.txt");
+    
+    Rules rules;
+    Pages pages;
+    while(getline(inputFile, inputLine))
+    {
+        if(!inputLine.empty())
+        {
+            if(inputLine.find('|') != string::npos)
+            {
+                parseLineInRules(inputLine, rules);
+            }
+            else
+            {
+                parseLine(inputLine, pages);
+            }
+        }
+    }
+)
+
+    inputFile.close();
+
+
     return 0;
 }
