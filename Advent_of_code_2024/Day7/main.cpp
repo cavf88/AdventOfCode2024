66192777e4c58a2073bb065a0d66d77dbdafb4be
unknown <cavf88@hotmail.com>
2024-12-07 11:45:59 -0500

    Day7: parsing

--- Advent_of_code_2024/Day7/main.cpp
+++ Advent_of_code_2024/Day7/main.cpp
@@ -5,7 +5,43 @@
 #include <fstream>
 #include <vector>
 
+using namespace std;
+typedef map<int, vector<int>> Equations;
+
+Equations parseInput(const string& inputName)
+{
+    string inputLine;
+    fstream inputFile(inputName);
+    Equations equations;
+    
+    while(getline(inputFile, inputLine))
+    {
+        int pos = inputLine.find(':');
+        const int result = atoi(inputLine.substr(0, pos).c_str());
+        pos++; //to avoid the first empty space after ":"
+        do
+        {
+            int newPos = inputLine.find(' ', pos+1);
+            int operand = 0;
+            if(newPos ==  string::npos)
+                operand = atoi(inputLine.substr(pos).c_str());
+            else
+                operand = atoi(inputLine.substr(pos, newPos).c_str());
+
+            if(operand > 0)
+                equations[result].push_back(operand);
+            pos = newPos;
+
+        }while(pos != string::npos);
+    }
+    inputFile.close();
+
+    return equations;
+}
+
 int main()
 {
+    const Equations equations = parseInput("input_test.txt");
+
     return 0;
 }
